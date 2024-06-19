cda1eac5b4bba7529b3690da7804ae57e5e6ecef
Claudia Torres <alet24665@gmail.com>
2024-06-19 06:52:53 -0300

    se agrego el login

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,5 +1,24 @@
+import { Container, Form, Button } from "react-bootstrap";
+
 const Login = () => {
-  return <section className="mainSection"></section>;
+  return (
+    <Container className="my-5 mainSection">
+      <h1 className="mb-4">Inicio de sesión</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Correo electronico:</Form.Label>
+          <Form.Control type="email" placeholder="claudia@mail.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Contraseña:</Form.Label>
+          <Form.Control type="password" placeholder="ingrese una contraseña" />
+        </Form.Group>
+        <Button variant="dark" type="submit">
+          Ingresar
+        </Button>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
