e7ce3152fac338bd95688d921092a260b4621be3
Cristian Diaz <diazcristian0210@gmail.com>
2024-06-26 12:57:21 -0300

    Di formato responsive a la imagen error

--- src/components/pages/Error404.jsx
+++ src/components/pages/Error404.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 const Error404 = () => {
     return (
         <section className='mainSection text-center'>
-            <img src = {error} alt="error 404"/>
+            <img src = {error} className='img-error' alt="error 404"/>
             <div>
-                <Link className='btn btn-primary mb-5'to="/">Volver al menú</Link>
+                <Link className='btn btn-primary mb-5 'to="/">Volver al menú</Link>
             </div>
         </section>
     );
